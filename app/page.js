cc83f4420c02d2ef9377477e50f51f0397e18f89
Muhammad usman <muhammadusman90712@gmail.com>
2024-05-08 12:38:54 +0500

    changes

--- app/page.js
+++ app/page.js
@@ -1,19 +1,30 @@
- "use client"
+//  "use client"
+
+//  import dynamic from "next/dynamic";
+// const BlogPage = dynamic(() => {
+//   import("./pages/blogs/page")
+//   import("./pages/blogs/[singleBlog]/page") 
+// }, {
+//   ssr: false,
+// });
  import Image from "next/image";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import Blogs from "./pages/blogs/page";
+// import { useRouter } from "next/navigation";
+// import React, { useEffect } from "react";
+// import SingleBlog from './pages/blogs/[singleBlog]/singleBlog';
 
 
 export default function Home() {
-  const router = useRouter();
+//   const router = useRouter();
 
-
-  useEffect(()=>{
-      router.push("/pages/login");
-  } , [])
+  // useEffect(()=>{
+  //     router.push("/pages/login");
+  // } , [])
   return (
     <>
     hello
+    {/* <Blog />
+    <SingleBlog/> */}
     </>
   );
 }
--- app/pages/blogs/[singleBlog]/page.jsx
+++ app/pages/blogs/[singleBlog]/page.jsx
@@ -1,16 +1,23 @@
-"use client"
-import SingleBlog from './singleBlog';
-const SingleBlogs = ({params}) =>{
+import dynamic from "next/dynamic";
+
+import SingleBlog from "./singleBlog";
+const MyComponent = dynamic(
+  async () => {
+    const module = await import("./singleBlog");
+    return module.MyComponent;
+  },
+  { ssr: false }
+);
+const SingleBlogs = ({ params }) => {
   //  if (typeof window !== undefined) {
   //    return;
   //  }
-  return(
+
+  return (
     <>
-    {
-      typeof window === undefined &&  <SingleBlog params={params}/>
-    }
- 
-  </>
-   )
-}
-export default SingleBlogs ;
+      {/* <SingleBlog params={params} /> */}
+      <MyComponent />
+    </>
+  );
+};
+export default SingleBlogs;
